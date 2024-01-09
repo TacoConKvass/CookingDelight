99d63c4225c2e4f49c6692949357a1c041a7e485
TacoConKvass <e_frun@o2.pl>
2024-01-09 08:29:24 +0100

    [ Addition ] - Food effect description

--- Common/Food/FoodCategory.cs
+++ Common/Food/FoodCategory.cs
@@ -2,12 +2,12 @@ namespace CookingDelight.Common;
 
 public enum FoodCategory
 {
-	Meat,
-	Seafood,
-	Fruit,
-	Vegetable,
-	Sweet,
-	Alcohol,
-	Other,
+	Meat,		// ++Endurance
+	Seafood,	// ++Damage
+	Fruit,		// ++Flight time
+	Vegetable,	// ++Max HP
+	Sweet,		// ++Speed
+	Alcohol,	// --Defense, +Attack speed, +Damage
+	Other,		
 	Spice
 }
