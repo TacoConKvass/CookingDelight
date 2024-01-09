f667a055258508a5a8b0a3a0697333507081aff8
TacoConKvass <efrun@o2.pl>
2024-01-09 15:32:08 +0100

    [ Addition ] - Effect level displayed in tooltip

--- Common/Food/GlobalFoodTooltip.cs
+++ Common/Food/GlobalFoodTooltip.cs
@@ -1,9 +1,19 @@
+using Humanizer;
+using System.Linq;
 using Terraria.Localization;
 
 namespace CookingDelight.Common;
 
 public class GlobalFoodTooltip : GlobalItem
 {
+	private static readonly Dictionary<int, string> foodLevelDisplay = new Dictionary<int, string>() {
+		{ 1, "I" },
+		{ 2, "II" },
+		{ 3, "III" },
+		{ 4, "IV" },
+		{ 5, "V" },
+	};
+
 	public override void ModifyTooltips(Item item, List<TooltipLine> tooltips) {
 		foreach (FoodCategory foodCategory in Enum.GetValues(typeof(FoodCategory))) {
 			if (VanillaFoodCategorizer.VanillaFoodByCategory[foodCategory].Contains(item.type)) {
@@ -13,7 +23,8 @@ public class GlobalFoodTooltip : GlobalItem
 
 			if (ModContent.GetModItem(item.type) is FoodItem food_item) {
 				if (food_item.Categories.Contains(foodCategory)) {
-					var line = new TooltipLine(Mod, "foodCategory", Language.GetTextValue($"Mods.CookingDelight.FoodCategories.{foodCategory}"));
+					string food_level = foodLevelDisplay[food_item.Categories.Where(element => element == foodCategory).Count()];
+					var line = new TooltipLine(Mod, "foodCategory", Language.GetTextValue($"Mods.CookingDelight.FoodCategories.{foodCategory}").FormatWith(food_level));
 					tooltips.Add(line);	
 				}
 			}
