ba4bba2226d5c24d3a63e3af3dd6ca7695ebbe9d
TacoConKvass <e_frun@o2.pl>
2024-01-10 11:54:01 +0100

    [ WIP ] - Food buff UI

---
+++ Common/UI/FoodBuffDisplay.cs
@@ -0,0 +1,13 @@
+using Microsoft.Xna.Framework.Graphics;
+using Terraria.GameContent.UI.Elements;
+using Terraria.UI;
+
+namespace CookingDelight.Common.UI;
+
+public class FoodBuffDisplay : UIState {
+
+	private UIElement area;
+	private UIImage[] activeBuffImages;
+	private UIText[] activeBuffDescriptions;
+
+}
