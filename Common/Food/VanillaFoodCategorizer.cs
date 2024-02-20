70eed142ed43d6c066008ccc283e17d95d8cfb20
TacoConKvass <e_frun@o2.pl>
2024-02-20 07:45:00 +0100

    [ Change ] - Spicy Pepper is only a vegetable now

--- Common/Food/VanillaFoodCategorizer.cs
+++ Common/Food/VanillaFoodCategorizer.cs
@@ -257,7 +257,6 @@ public class VanillaFoodCategorizer : GlobalItem
 			ItemID.Fireblossom,
 			ItemID.Moonglow,
 			ItemID.Shiverthorn,
-			ItemID.SpicyPepper,
 			ItemID.StarAnise,
 			ItemID.Waterleaf
 		});
