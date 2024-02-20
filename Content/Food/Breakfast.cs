26b2cea9b986ebb5a6866750ae39ff866ccc5cbc
TacoConKvass <e_frun@o2.pl>
2024-02-20 07:25:19 +0100

    [ Change ] - Additional OnSpawn logic

--- Content/Food/Breakfast.cs
+++ Content/Food/Breakfast.cs
@@ -1,8 +1,10 @@
+using CookingDelight.Common.EntitySources;
+
 namespace CookingDelight.Content.Food;
 
 public class Breakfast : FoodItem 
 {
-	public override List<FoodCategory> Categories { get; set; } = new List<FoodCategory>() { };
+	public override List<FoodCategory> Categories { get; set; } = new List<FoodCategory>() { FoodCategory.Meat, FoodCategory.Meat, FoodCategory.Other };
 
 	public override int BuffTime { get; set; } = 3.Minutes();
 
@@ -15,5 +17,14 @@ public class Breakfast : FoodItem
 		Item.useStyle = ItemUseStyleID.EatFood;
 		Item.useAnimation = 30;
 		Item.useTime = 30;
+	}
+
+	public override void OnSpawn(IEntitySource source) {
+		if (source is not ItemSource_Cooking) {
+			return;
+		}
+
+		base.OnSpawn(source);
+		Categories.Add(FoodCategory.Meat);
 	}
 }
